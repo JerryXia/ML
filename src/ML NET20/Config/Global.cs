cd78ae9d924c029ff51d82e8c508209a11a5621a
JerryXia <gqkzwy@gmail.com>
2013-12-05 11:36:46 +0800

    add some base library

---
+++ src/ML NET20/Config/Global.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+
+namespace ML.Config
+{
+    public class Global
+    {
+        #region App and Web Directory
+
+        /// <summary>
+        /// App的目录
+        /// </summary>
+        public static string AppDir
+        {
+            get
+            {
+                return System.Environment.CurrentDirectory;
+            }
+        }
+        /// <summary>
+        /// Web应用程序的目录
+        /// </summary>
+        public static string WebDir
+        {
+            get
+            {
+                string s = HttpRuntime.AppDomainAppPath;
+                return System.AppDomain.CurrentDomain.BaseDirectory;
+            }
+        }
+
+        #endregion
+    }
+}

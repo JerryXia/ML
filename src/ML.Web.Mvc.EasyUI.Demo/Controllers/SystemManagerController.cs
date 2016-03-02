61abd3b12a5b0d523b910941d6522c8a5581d418
JerryXia <gqkzwy@gmail.com>
2016-03-02 23:06:54 +0800

    static files remove

---
+++ src/ML.Web.Mvc.EasyUI.Demo/Controllers/SystemManagerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ML.Web.Mvc.EasyUI.Demo.Controllers
+{
+    public class SystemManagerController : Controller
+    {
+
+        [HttpPost]
+        public ActionResult Log()
+        {
+            return View();
+        }
+
+    }
+}
